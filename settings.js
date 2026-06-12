015c995af914fec99bf9eee0b96c5fc9899adc1b
sshdgxg36366 <oshadamd6@gmail.com>
2026-06-12 15:14:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+qconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=6ecf59a81da3ae64' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=62b7d798cda4c9ee' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
