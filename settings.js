9da908db9dc6ac6a5075752d604aba707225dc77
sshdgxg36366 <oshadamd6@gmail.com>
2026-06-26 18:57:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,10 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3bf5be13f37b5453' : process.env.SESSION_ID,
+MONGODB: process.env.MONGODB_URI === undefined ? 'mongodb+srv://oshadamd05_db_user:LzWjINfr32GpUo6v@cluster0.mu6qfvw.mongodb.net/?appName=Cluster0' : process.env.MONGODB_URI,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASITHA-MD~3bf5be13f37b5453' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.SESSION_NAME === undefined ? "asitha" : process.env.SESSION_NAME,
+
 };
+
